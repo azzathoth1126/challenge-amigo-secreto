251d7b249c42d2d49c1818d7b1a9c0ddb016078a
azzathoth1126 <alexis.mta2020@gmail.com>
2025-08-17 14:32:02 -0600

    Actualiza la funcion Agregar Amigos

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+
+function agregarAmigo() {
+
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    validacionCampo(nombreAmigo);
+
+    limpiarCaja();
+
+    return;
+}
+
+
+function limpiarCaja() {
+    let nombreEnCaja = document.getElementById('amigo').value= '';
+}
+
+
+function validacionCampo(nombre) {
+
+    if (nombre == ''){
+        alert('Ingresa un nombre valido');
+        return;
+    } else {
+        amigos.push(nombre);
+        //console.log(amigos);
+    }
+
+    return;
+}
