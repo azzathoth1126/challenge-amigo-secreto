52389e514468592752c1116cbe6046f8e87caf87
azzathoth1126 <alexis.mta2020@gmail.com>
2025-08-18 11:25:25 -0600

    Actualiza funciones pedidas

--- app.js
+++ app.js
@@ -1,61 +1,60 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Lista de nombres
 let amigos = [];
 
+// Actualiza la lista en pantalla
 function actualizarLista() {
+
+    //Selecciona la lista donde agregamos nombres por el id
     let lista = document.getElementById("listaAmigos");
-    
-    let nuevoElemento = document.createElement("li");
 
-    nuevoElemento.textContent = amigos[amigos.length - 1];
+    //limpia la lista
+    lista.innerHTML = "";
 
-    lista.appendChild(nuevoElemento);
+    //Agrega a la lista los nombres guardados en un arreglo
+    for (let i = 0; i < amigos.length; i++) {
+        let nuevoElemento = document.createElement("li");
+         nuevoElemento.textContent = amigos[i];
+         lista.appendChild(nuevoElemento);
+    }
 
     return;
 }
 
 
+// Agrega nombres a la lista
 function agregarAmigo() {
+    
+    //Toma el nombre de la caja que identifica con el id
     let nombreAmigo = document.getElementById('amigo').value;
 
+    //Llama a la función validacionCampo enviando como parametro el nombre en la caja
     validacionCampo(nombreAmigo);
 
-    limpiarCaja();
+    //Limpia la caja donde escriben nombres
+    let nombreEnCaja = document.getElementById('amigo').value= '';
 
     return;
 }
 
 
-function limpiarCaja() {
-    let nombreEnCaja = document.getElementById('amigo').value= '';
-}
-
-
+// Valida si efectivamente es un nombre y no esta vacia la caja de nombres
 function validacionCampo(nombre) {
 
+    //Verica que no sea una cadena vacia
     if (nombre == ''){
+        //Envia una alerta para que escriba un nombre
         alert('Ingresa un nombre valido');
         return;
 
-    } else if (repetidos(nombre)) {
+    } else {
+        //Agrea el nombre a la lista y la actualiza en pantalla
         //console.log(amigos);
-
         amigos.push(nombre);
         actualizarLista();
 
     }
 
     return;
-}
-
-
-function repetidos(nombre) {
-    for (let i = 0; i < amigos.length; i++){
-        if (nombre == amigos[i]) {
-            alert('Nombre duplicado.');
-            return false;
-        }
-    }
-
-    return true;
 }
